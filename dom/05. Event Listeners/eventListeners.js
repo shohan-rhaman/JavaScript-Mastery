9d684620875414f6304cc6b7b1fa69a120970739
shohan-rhaman <shohanurrahman.contact@gmail.com>
2026-07-31 00:35:07 +0600

    mini challange

--- dom/05. Event Listeners/eventListeners.js
+++ dom/05. Event Listeners/eventListeners.js
@@ -13,3 +13,15 @@ addToCartBtn.addEventListener("click", ()=>{
     addToCartBtn.textContent = "Added to cart";
     addToCartBtn.classList.add("bg-green-600")
 })
+
+
+// ====================== Challange ==========================
+
+// Q: when the button is clicked: 1. Text becomes "Added to Cart". 2. Class "active" is added
+
+const buy = document.querySelector("#buy");
+
+buy.addEventListener("click", ()=>{
+    buy.textContent = "Added to Cart";
+    buy.classList.add("active")
+})
