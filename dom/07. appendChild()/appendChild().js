5b34b4f004095ba0f21cbd40eebaf7161fe1ccf2
shohan-rhaman <shohanurrahman.contact@gmail.com>
2026-08-02 14:51:11 +0600

    Shopify Example for appendChild

--- dom/07. appendChild()/appendChild().js
+++ dom/07. appendChild()/appendChild().js
@@ -5,3 +5,18 @@ const p = document.createElement("p");
 p.textContent = "Hello";
 
 container.appendChild(p);
+
+
+// ====================== Shopify Example ============================ //
+const products = {
+    name: "Nike Air Max"
+};
+
+const productsList = document.querySelector("#product-list")
+
+const div = document.createElement("div");
+
+div.textContent = products.name;
+
+productsList.appendChild(div)
+
