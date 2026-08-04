e354ed661aa1ab69164303728d6acd4d8929d00d
shohan-rhaman <shohanurrahman.contact@gmail.com>
2026-08-04 14:11:49 +0600

    Change the UI on Click

--- DOM Events/01. addEventListener()/addEventListener().js
+++ DOM Events/01. addEventListener()/addEventListener().js
@@ -1,5 +1,23 @@
-const Button = document.querySelector("#buy-btn")
+/* const Button = document.querySelector("#buy-btn")
 
 Button.addEventListener("click", ()=>{
     console.log("button clicked");
 })
+
+
+const btn = document.querySelector("#btn")
+
+btn.addEventListener("click", ()=>{
+    console.log("Button Clicked!")
+}) */
+
+
+
+// ================== Change the UI on Click ======================== //
+
+const status = document.querySelector("#status");
+const btn = document.querySelector("#btn");
+
+btn.addEventListener("click", () => {
+    status.textContent = "In Stock";
+});
